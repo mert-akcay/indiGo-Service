e8fdc315c3c2be92f7fa2d4cedd5eccc4ddb5a7a
Mert Akcay <akcaymert603@gmail.com>
2022-05-17 15:34:08 +0300

    Layout, login and register pages added

---
+++ indiGo.Web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace indiGo.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
